2ee92352ec24b46199172aa3c2cd6b5516308775
Tanmoy <tanmoychakraborty0041383@gmail.com>
2024-11-18 12:39:48 +0530

    all files

---
+++ app.js
@@ -0,0 +1,45 @@
+let user = 0;
+let com = 0;
+
+const choices = document.querySelectorAll(".choice");
+let msg = document.querySelector(".msg");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
