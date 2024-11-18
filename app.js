ac1b84a48c7fdc3aeb77ec3843e7f49ff8425e2f
Tanmoy <tanmoychakraborty0041383@gmail.com>
2024-11-18 19:33:36 +0530

    all files

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ let com = 0;
 const choices = document.querySelectorAll(".choice");
 let msg = document.querySelector(".msg");
 
-
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        console.log("checked!")
+    })
+}) 
 
 
 
